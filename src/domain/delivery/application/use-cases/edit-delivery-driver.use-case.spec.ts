13c063cf25fccf75de3b624c054cccf70c138cf0
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-12 19:26:31 -0300

    feat(core): Adição tratamento de erro para usuário não encontrado

--- src/domain/delivery/application/use-cases/edit-delivery-driver.use-case.spec.ts
+++ src/domain/delivery/application/use-cases/edit-delivery-driver.use-case.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryDeliveryDriversRepository } from 'test/repositories/in-memory-delivery-drivers.repository'
 import { EditDeliveryDriverUseCase } from './edit-delivery-driver.use-case'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
 
 const makeSut = () => {
   const deliveryDriversRepository = new InMemoryDeliveryDriversRepository()
@@ -11,5 +12,25 @@ describe('EditDeliveryDriverUseCase', () => {
   it('should be defined', () => {
     const { sut } = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it('should return ResourceNotFoundError if delivery driver does not exists', async () => {
+    const { sut } = makeSut()
+
+    const output = await sut.execute({
+      deliveryDriverId: 'non-existing-id',
+    })
+
+    expect(output.value).toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should is left if delivery driver does not exists', async () => {
+    const { sut } = makeSut()
+
+    const output = await sut.execute({
+      deliveryDriverId: 'non-existing-id',
+    })
+
+    expect(output.isLeft()).toBeTruthy()
   })
 })
--- src/domain/delivery/application/use-cases/edit-delivery-driver.use-case.ts
+++ src/domain/delivery/application/use-cases/edit-delivery-driver.use-case.ts
@@ -1,9 +1,11 @@
-import { Either } from '@/core/either'
+import { Either, left } from '@/core/either'
 import { DeliveryDriversRepository } from '@/core/repositories/delivery-drivers.repository'
 import { DeliveryDriver } from '../../enterprise/entities/delivery-driver'
 import { ResourceNotFoundError } from './errors/resource-not-found.error'
 
-export type EditDeliveryDriverUseCaseInput = {}
+export type EditDeliveryDriverUseCaseInput = {
+  deliveryDriverId: string
+}
 
 export type EditDeliveryDriverUseCaseOutput = Either<
   ResourceNotFoundError,
@@ -17,7 +19,16 @@ export class EditDeliveryDriverUseCase {
     private readonly deliveryDriversRepository: DeliveryDriversRepository,
   ) {}
 
-  async execute({}: EditDeliveryDriverUseCaseInput): Promise<EditDeliveryDriverUseCaseOutput> {
+  async execute({
+    deliveryDriverId,
+  }: EditDeliveryDriverUseCaseInput): Promise<EditDeliveryDriverUseCaseOutput> {
+    const deliveryDriver =
+      await this.deliveryDriversRepository.findById(deliveryDriverId)
+
+    if (!deliveryDriver) {
+      return left(new ResourceNotFoundError(deliveryDriverId))
+    }
+
     return {} as any
   }
 }
