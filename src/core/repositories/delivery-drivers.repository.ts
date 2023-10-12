543b0c9d7cbfadc6ff31cad8bdd46aa046255e9f
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-12 17:55:20 -0300

    feat: Pesquisa entregadores

--- src/core/repositories/delivery-drivers.repository.ts
+++ src/core/repositories/delivery-drivers.repository.ts
@@ -3,4 +3,5 @@ import { DeliveryDriver } from '@/domain/delivery/enterprise/entities/delivery-d
 export abstract class DeliveryDriversRepository {
   abstract create(deliverydriver: DeliveryDriver): Promise<void>
   abstract findByCPF(cpf: string): Promise<DeliveryDriver | null>
+  abstract findMany(): Promise<DeliveryDriver[]>
 }
