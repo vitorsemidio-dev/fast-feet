781472ce9fce58df0700392c01e5959a711c1934
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-12 13:42:48 -0300

    refactor: Utilização factory para construção dos parâmetros no teste de entregador

--- src/domain/delivery/enterprise/entities/delivery-driver.spec.ts
+++ src/domain/delivery/enterprise/entities/delivery-driver.spec.ts
@@ -1,67 +1,72 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
-import { DeliveryDriver } from '@/domain/delivery/enterprise/entities/delivery-driver'
+import {
+  DeliveryDriver,
+  DeliveryDriverProps,
+} from '@/domain/delivery/enterprise/entities/delivery-driver'
 import { Roles } from '@/domain/delivery/enterprise/entities/roles.enum'
 import { Role } from '@prisma/client'
-import { CPF } from './value-objects/cpf'
+import { makeDeliveryDriver } from 'test/factories/delivery-driver.factory'
+
+const makeSutInput = (
+  override: Partial<DeliveryDriverProps> = {},
+  _id?: UniqueEntityId,
+) => {
+  const deliveryDriver = makeDeliveryDriver(override, _id)
+  const props = {
+    cpf: deliveryDriver.cpf,
+    name: deliveryDriver.name,
+    password: deliveryDriver.password,
+    role: deliveryDriver.role,
+  }
+  const id = deliveryDriver.id
+  return {
+    props,
+    id,
+  }
+}
 
 describe('DeliveryDriver', () => {
-  it('should be able to create new deliverydriver', () => {
-    const deliverydriver = DeliveryDriver.create({
-      cpf: CPF.makeRandom(),
-      name: 'DeliveryDriver',
-      password: '123456',
-    })
-    expect(deliverydriver).toBeDefined()
+  it('should be able to create new delivery driver', () => {
+    const { props } = makeSutInput()
+    const deliveryDriver = DeliveryDriver.create(props)
+    expect(deliveryDriver).toBeDefined()
   })
 
-  it('should be able to generate id when create new deliverydriver', () => {
-    const deliverydriver = DeliveryDriver.create({
-      cpf: CPF.makeRandom(),
-      name: 'Delivery Driver',
-      password: '123456',
-    })
-    expect(deliverydriver.id).toBeDefined()
-    expect(deliverydriver.id.toString()).toBeDefined()
+  it('should be able to generate id when create new delivery driver', () => {
+    const { props } = makeSutInput()
+    const deliveryDriver = DeliveryDriver.create(props)
+    expect(deliveryDriver.id).toBeDefined()
+    expect(deliveryDriver.id.toString()).toBeDefined()
   })
 
-  it('should be able to provide id when create new deliverydriver', () => {
-    const deliverydriver = DeliveryDriver.create(
-      {
-        cpf: CPF.makeRandom(),
-        name: 'Delivery Driver',
-        password: '123456',
-      },
+  it('should be able to provide id when create new delivery driver', () => {
+    const { props, id } = makeSutInput(
+      {},
       new UniqueEntityId('delivery-driver-id'),
     )
-    expect(deliverydriver.id.toString()).toEqual('delivery-driver-id')
+    const deliveryDriver = DeliveryDriver.create(props, id)
+    expect(deliveryDriver.id.toString()).toEqual('delivery-driver-id')
   })
 
   it('should be able to call toJson method', () => {
-    const cpf = CPF.makeRandom()
-    const deliverydriver = DeliveryDriver.create(
-      {
-        cpf,
-        name: 'Delivery Driver',
-        password: '123456',
-      },
+    const { props } = makeSutInput()
+    const deliveryDriver = DeliveryDriver.create(
+      props,
       new UniqueEntityId('delivery-driver-id'),
     )
     const expected = expect.objectContaining({
       id: 'delivery-driver-id',
-      cpf: cpf.value,
-      name: 'Delivery Driver',
-      password: '123456',
+      cpf: props.cpf.value,
+      name: props.name,
+      password: props.password,
       role: Role.DELIVERY_DRIVER,
     })
-    expect(deliverydriver.toJson()).toEqual(expected)
+    expect(deliveryDriver.toJson()).toEqual(expected)
   })
 
   it('should automatically assign the "DELIVERY_DRIVER" role when creating a new delivery driver user', () => {
-    const deliverydriver = DeliveryDriver.create({
-      cpf: CPF.makeRandom(),
-      name: 'Delivery Driver',
-      password: '123456',
-    })
-    expect(deliverydriver.role).toEqual(Roles.DELIVERY_DRIVER)
+    const { props } = makeSutInput()
+    const deliveryDriver = DeliveryDriver.create(props)
+    expect(deliveryDriver.role).toEqual(Roles.DELIVERY_DRIVER)
   })
 })
