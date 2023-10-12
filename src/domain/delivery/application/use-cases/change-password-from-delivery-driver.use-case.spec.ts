faeab4eb7f271324f94933c055ec0cb1612b915d
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-12 20:20:21 -0300

    feat(core): Adição tratamento de erro para usuário não encontrado

--- src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.spec.ts
+++ src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.spec.ts
@@ -1,15 +1,46 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { InMemoryDeliveryDriversRepository } from 'test/repositories/in-memory-delivery-drivers.repository'
 import { ChangePasswordFromDeliveryDriverUseCase } from './change-password-from-delivery-driver.use-case'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
 
 const makeSut = () => {
-  const sut = new ChangePasswordFromDeliveryDriverUseCase()
+  const deliveryDriversRepository = new InMemoryDeliveryDriversRepository()
+  const sut = new ChangePasswordFromDeliveryDriverUseCase(
+    deliveryDriversRepository,
+  )
   return {
     sut,
+    deliveryDriversRepository,
+  }
+}
+
+const makeSutInput = () => {
+  return {
+    deliveryDriverId: new UniqueEntityId().toString(),
   }
 }
 
 describe('ChangePasswordFromDeliveryDriverUseCase', () => {
+  let sut: ChangePasswordFromDeliveryDriverUseCase
+  let deliveryDriversRepository: InMemoryDeliveryDriversRepository
+
+  beforeEach(() => {
+    const dependencies = makeSut()
+    sut = dependencies.sut
+    deliveryDriversRepository = dependencies.deliveryDriversRepository
+  })
+
   it('should be defined', () => {
     const { sut } = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it('should return ResourceNotFoundError if delivery driver does not exists', async () => {
+    const input = makeSutInput()
+
+    const output = await sut.execute(input)
+
+    expect(output.value).toBeInstanceOf(ResourceNotFoundError)
+    expect(output.isLeft()).toBeTruthy()
   })
 })
--- src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.ts
+++ src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.ts
@@ -1,15 +1,29 @@
-import { Either } from '@/core/either'
+import { Either, left } from '@/core/either'
+import { DeliveryDriversRepository } from '@/core/repositories/delivery-drivers.repository'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
 
-type ChangePasswordFromDeliveryDriverUseCaseInput = {}
+type ChangePasswordFromDeliveryDriverUseCaseInput = {
+  deliveryDriverId: string
+}
 
-type ChangePasswordFromDeliveryDriverUseCaseOutput = Either<void, void>
+type ChangePasswordFromDeliveryDriverUseCaseOutput = Either<
+  ResourceNotFoundError,
+  void
+>
 
 export class ChangePasswordFromDeliveryDriverUseCase {
-  constructor() {}
+  constructor(
+    private readonly deliveryDriversRepository: DeliveryDriversRepository,
+  ) {}
 
-  public async execute(
-    request: ChangePasswordFromDeliveryDriverUseCaseInput,
-  ): Promise<ChangePasswordFromDeliveryDriverUseCaseOutput> {
+  public async execute({
+    deliveryDriverId,
+  }: ChangePasswordFromDeliveryDriverUseCaseInput): Promise<ChangePasswordFromDeliveryDriverUseCaseOutput> {
+    const deliveryDriver =
+      await this.deliveryDriversRepository.findById(deliveryDriverId)
+    if (!deliveryDriver) {
+      return left(new ResourceNotFoundError(deliveryDriverId))
+    }
     return {} as any
   }
 }
