23fed3931a1b8a816bc88c31f7b94c2c71694551
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-12 21:07:40 -0300

    feat(core): Atualização da senha do usuário com hash no banco de dados

--- src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.spec.ts
+++ src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.spec.ts
@@ -13,16 +13,20 @@ import { ResourceNotFoundError } from './errors/resource-not-found.error'
 import { WrongPasswordError } from './errors/wrong-password.error'
 
 const makeSut = () => {
-  const hashComparer = new FakeHasher()
+  const fakeHasher = new FakeHasher()
+  const hashComparer = fakeHasher
+  const hashGenerator = fakeHasher
   const deliveryDriversRepository = new InMemoryDeliveryDriversRepository()
   const sut = new ChangePasswordFromDeliveryDriverUseCase(
     deliveryDriversRepository,
     hashComparer,
+    hashGenerator,
   )
   return {
     sut,
     deliveryDriversRepository,
     hashComparer,
+    hashGenerator,
   }
 }
 
@@ -32,6 +36,7 @@ const makeSutInput = (
   return {
     deliveryDriverId: new UniqueEntityId().toString(),
     oldPassword: fakerPtBr.internet.password(),
+    newPassword: fakerPtBr.internet.password(),
     ...overrider,
   }
 }
@@ -47,12 +52,37 @@ describe('ChangePasswordFromDeliveryDriverUseCase', () => {
     sut = dependencies.sut
     deliveryDriversRepository = dependencies.deliveryDriversRepository
     hashComparer = dependencies.hashComparer
-    hashGenerator = new FakeHasher()
+    hashGenerator = dependencies.hashGenerator
   })
 
   it('should be defined', () => {
     const { sut } = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it('should be able to update delivery driver password and persiste on database new password hashed', async () => {
+    const deliveryDriver = makeDeliveryDriver({
+      password: 'old-password',
+    })
+    const deliveryDriverOnDB = deliveryDriver.clone()
+    deliveryDriverOnDB.password = await hashGenerator.hash('old-password')
+    await deliveryDriversRepository.create(deliveryDriverOnDB)
+
+    const input = makeSutInput({
+      deliveryDriverId: deliveryDriver.id.toString(),
+      oldPassword: deliveryDriver.password,
+      newPassword: 'new-password',
+    })
+
+    const output = await sut.execute(input)
+
+    expect(deliveryDriversRepository.itens[0].password).not.toBe(
+      await hashGenerator.hash('old-password'),
+    )
+    expect(deliveryDriversRepository.itens[0].password).toBe(
+      await hashGenerator.hash('new-password'),
+    )
+    expect(output.isRight()).toBeTruthy()
   })
 
   it('should is right when provide correct old password', async () => {
--- src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.ts
+++ src/domain/delivery/application/use-cases/change-password-from-delivery-driver.use-case.ts
@@ -1,12 +1,14 @@
 import { Either, left, right } from '@/core/either'
 import { DeliveryDriversRepository } from '@/core/repositories/delivery-drivers.repository'
 import { HashComparer } from '@/domain/delivery/application/cryptography/hash-comparer'
+import { HashGenerator } from '../cryptography/hash-generator'
 import { ResourceNotFoundError } from './errors/resource-not-found.error'
 import { WrongPasswordError } from './errors/wrong-password.error'
 
 export type ChangePasswordFromDeliveryDriverUseCaseInput = {
   deliveryDriverId: string
   oldPassword: string
+  newPassword: string
 }
 
 export type ChangePasswordFromDeliveryDriverUseCaseOutput = Either<
@@ -18,11 +20,13 @@ export class ChangePasswordFromDeliveryDriverUseCase {
   constructor(
     private readonly deliveryDriversRepository: DeliveryDriversRepository,
     private readonly hashComparer: HashComparer,
+    private readonly hashGenerator: HashGenerator,
   ) {}
 
   public async execute({
     deliveryDriverId,
     oldPassword,
+    newPassword,
   }: ChangePasswordFromDeliveryDriverUseCaseInput): Promise<ChangePasswordFromDeliveryDriverUseCaseOutput> {
     const deliveryDriver =
       await this.deliveryDriversRepository.findById(deliveryDriverId)
@@ -36,6 +40,9 @@ export class ChangePasswordFromDeliveryDriverUseCase {
     if (!passwordMatch) {
       return left(new WrongPasswordError())
     }
+
+    deliveryDriver.password = await this.hashGenerator.hash(newPassword)
+    await this.deliveryDriversRepository.update(deliveryDriver)
 
     return right(undefined)
   }
