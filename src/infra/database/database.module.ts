0f4dab20006b9fcae6e774d120e1607371743a68
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-15 10:37:20 -0300

    chore(infra): Adicionado novos repositórios ao DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,12 @@
 import { AdministratorsRepository } from '@/core/repositories/administrators.repositories'
 import { DeliveryDriversRepository } from '@/core/repositories/delivery-drivers.repository'
+import { OrdersRepository } from '@/core/repositories/orders.repository'
+import { RecipientsRepository } from '@/core/repositories/recipients.repository'
 import { Module } from '@nestjs/common'
 import { PrismaAdministratorsRepository } from './repositories/prisma-administrators.repository'
 import { PrismaDeliveryDriversRepository } from './repositories/prisma-delivery-drivers.repository'
+import { PrismaOrdersRepository } from './repositories/prisma-orders.repository'
+import { PrismaRecipientsRepository } from './repositories/prisma-recipients.repository'
 import { PrismaService } from './services/prisma.service'
 
 @Module({
@@ -16,7 +20,21 @@ import { PrismaService } from './services/prisma.service'
       provide: DeliveryDriversRepository,
       useClass: PrismaDeliveryDriversRepository,
     },
+    {
+      provide: OrdersRepository,
+      useClass: PrismaOrdersRepository,
+    },
+    {
+      provide: RecipientsRepository,
+      useClass: PrismaRecipientsRepository,
+    },
   ],
-  exports: [PrismaService, AdministratorsRepository, DeliveryDriversRepository],
+  exports: [
+    PrismaService,
+    AdministratorsRepository,
+    DeliveryDriversRepository,
+    OrdersRepository,
+    RecipientsRepository,
+  ],
 })
 export class DatabaseModule {}
