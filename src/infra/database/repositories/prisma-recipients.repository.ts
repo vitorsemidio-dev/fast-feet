b21a60bbe74718c75e9c75bcb0aded4cf382778a
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-15 12:04:36 -0300

    feat(infra): Implementação PrismaRecipientsRepository

--- src/infra/database/repositories/prisma-recipients.repository.ts
+++ src/infra/database/repositories/prisma-recipients.repository.ts
@@ -1,15 +1,30 @@
 import { RecipientsRepository } from '@/core/repositories/recipients.repository'
 import { Recipient } from '@/domain/delivery/enterprise/entities/recipient'
+import { UserRoles } from '@/domain/delivery/enterprise/entities/user-roles.enum'
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../services/prisma.service'
 
 @Injectable()
 export class PrismaRecipientsRepository implements RecipientsRepository {
   constructor(private readonly prismaService: PrismaService) {}
-  findByCPF(cpf: string): Promise<Recipient | null> {
-    throw new Error('Method not implemented.')
+
+  async findByCPF(cpf: string): Promise<Recipient | null> {
+    const recipient = await this.prismaService.user.findUnique({
+      where: { cpf, role: UserRoles.RECIPIENT },
+    })
+
+    if (!recipient) return null
+
+    return recipient as any
   }
-  findById(id: string): Promise<Recipient | null> {
-    throw new Error('Method not implemented.')
+
+  async findById(id: string): Promise<Recipient | null> {
+    const recipient = await this.prismaService.user.findUnique({
+      where: { id, role: UserRoles.RECIPIENT },
+    })
+
+    if (!recipient) return null
+
+    return recipient as any
   }
 }
