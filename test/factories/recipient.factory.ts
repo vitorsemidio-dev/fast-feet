d658351bd03f62209c3c68afc7e2aecdcc7ece8d
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-15 13:32:44 -0300

    test(infra) Criação RecipientFactory para auxiliar nos testes e2e

--- test/factories/recipient.factory.ts
+++ test/factories/recipient.factory.ts
@@ -4,6 +4,9 @@ import {
   RecipientProps,
 } from '@/domain/delivery/enterprise/entities/recipient'
 import { CPF } from '@/domain/delivery/enterprise/entities/value-objects/cpf'
+import { PrismaRecipientMapper } from '@/infra/database/mappers/prisma-recipient.mapper'
+import { PrismaService } from '@/infra/database/services/prisma.service'
+import { Injectable } from '@nestjs/common'
 import { fakerPtBr } from 'test/utils/faker'
 
 export function makeRecipient(
@@ -21,4 +24,21 @@ export function makeRecipient(
   )
 
   return recipient
+}
+
+@Injectable()
+export class RecipientFactory {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async make(
+    override: Partial<RecipientProps> = {},
+    id?: UniqueEntityId,
+  ): Promise<Recipient> {
+    const recipient = makeRecipient(override, id)
+
+    const data = PrismaRecipientMapper.toPersistence(recipient)
+    await this.prismaService.user.create({ data })
+
+    return recipient
+  }
 }
