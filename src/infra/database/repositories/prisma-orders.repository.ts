febd0a26b028c14a6ed30a6d94bfa65b141954eb
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-15 12:05:21 -0300

    feat(infra): Implementação PrismaOrdersRepository

--- src/infra/database/repositories/prisma-orders.repository.ts
+++ src/infra/database/repositories/prisma-orders.repository.ts
@@ -1,18 +1,32 @@
 import { OrdersRepository } from '@/core/repositories/orders.repository'
 import { Order } from '@/domain/delivery/enterprise/entities/order'
 import { Injectable } from '@nestjs/common'
+import { PrismaOrderMapper } from '../mappers/prisma-order.mapper'
 import { PrismaService } from '../services/prisma.service'
 
 @Injectable()
 export class PrismaOrdersRepository implements OrdersRepository {
   constructor(private readonly prismaService: PrismaService) {}
-  create(order: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async create(order: Order): Promise<void> {
+    const data = PrismaOrderMapper.toPersistence(order)
+    await this.prismaService.order.create({ data })
   }
-  findById(id: string): Promise<Order | null> {
-    throw new Error('Method not implemented.')
+
+  async findById(id: string): Promise<Order | null> {
+    const order = await this.prismaService.order.findUnique({ where: { id } })
+
+    if (!order) return null
+
+    return PrismaOrderMapper.toDomain(order)
   }
-  save(order: Order): Promise<void> {
-    throw new Error('Method not implemented.')
+
+  async save(order: Order): Promise<void> {
+    const data = PrismaOrderMapper.toPersistence(order)
+
+    await this.prismaService.order.update({
+      where: { id: order.id.toString() },
+      data,
+    })
   }
 }
