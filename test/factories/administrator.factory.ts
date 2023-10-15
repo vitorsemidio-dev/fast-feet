f47a9992bba944e24f6942a437bcc4b4f9f79d88
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-15 13:25:41 -0300

    test(infra) Criação AdministratorFactory para auxiliar nos testes e2e

--- test/factories/administrator.factory.ts
+++ test/factories/administrator.factory.ts
@@ -4,6 +4,9 @@ import {
   AdministratorProps,
 } from '@/domain/delivery/enterprise/entities/administrator'
 import { CPF } from '@/domain/delivery/enterprise/entities/value-objects/cpf'
+import { PrismaAdministratorMapper } from '@/infra/database/mappers/prisma-adminisitrator.mapper'
+import { PrismaService } from '@/infra/database/services/prisma.service'
+import { Injectable } from '@nestjs/common'
 import { fakerPtBr } from 'test/utils/faker'
 
 export function makeAdministrator(
@@ -21,4 +24,21 @@ export function makeAdministrator(
   )
 
   return administrator
+}
+
+@Injectable()
+export class AdministratorFactory {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async make(
+    override: Partial<AdministratorProps> = {},
+    id?: UniqueEntityId,
+  ): Promise<Administrator> {
+    const administrator = makeAdministrator(override, id)
+
+    const data = PrismaAdministratorMapper.toPersistence(administrator)
+    await this.prismaService.user.create({ data })
+
+    return administrator
+  }
 }
