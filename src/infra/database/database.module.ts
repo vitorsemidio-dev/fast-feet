bcb9ad9576b479aceacf9aff89f5bf48358fb773
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-10-14 22:12:06 -0300

    feat(infra): Adicionado PrismaDeliveryDriversRepository ao módulo de DatabaseModule

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,8 @@
 import { AdministratorsRepository } from '@/core/repositories/administrators.repositories'
+import { DeliveryDriversRepository } from '@/core/repositories/delivery-drivers.repository'
 import { Module } from '@nestjs/common'
 import { PrismaAdministratorsRepository } from './repositories/prisma-administrators.repository'
+import { PrismaDeliveryDriversRepository } from './repositories/prisma-delivery-drivers.repository'
 import { PrismaService } from './services/prisma.service'
 
 @Module({
@@ -10,7 +12,11 @@ import { PrismaService } from './services/prisma.service'
       provide: AdministratorsRepository,
       useClass: PrismaAdministratorsRepository,
     },
+    {
+      provide: DeliveryDriversRepository,
+      useClass: PrismaDeliveryDriversRepository,
+    },
   ],
-  exports: [PrismaService, AdministratorsRepository],
+  exports: [PrismaService, AdministratorsRepository, DeliveryDriversRepository],
 })
 export class DatabaseModule {}
